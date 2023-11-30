7cfa9348e920b81701654011bc8a99dd87ecb023
Felix Garcia <felix.garcia@contractor.temenos.com>
2023-11-29 22:21:31 -0500

    Adding modal to create more servers! it's looks great :D

--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -1,9 +1,13 @@
 'use client'
 
 import { Plus } from 'lucide-react'
-import { ActionsTooltip } from '../actions-tooltip'
+import { ActionsTooltip } from '@/components/actions-tooltip'
+import { useModal } from '@/hooks/use-modal-store';
 
 export const NavigationAction = () => {
+
+  const { onOpen } = useModal();
+
   return (
     <div>
       <ActionsTooltip 
@@ -11,7 +15,10 @@ export const NavigationAction = () => {
         side='right'
         align='center'
       >
-        <button className='group flex items-center'>
+        <button 
+          onClick={() => onOpen('createServer')}
+          className='group flex items-center'
+        >
           <div className='flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-emerald-500'>
             <Plus className='group-hover:text-white transition text-emerald-500' size={25}></Plus>
           </div>
