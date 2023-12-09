4094b490f2f402baea0af1be0d98cc14022ac273
Felix Garcia <felix.garcia@contractor.temenos.com>
2023-12-08 21:10:49 -0500

    Add SocketProvider and SocketIndicator components

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { cn } from '@/lib/utils';
 
 import { ThemeProvider } from '@/components/providers/theme-provider';
 import { ModalProvider } from '@/components/providers/modal-provider';
+import { SocketProvider } from '@/components/providers/socket-provider';
 
 const font = Open_Sans({ subsets: ['latin'] });
 
@@ -26,8 +27,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 						enableSystem={false}
 						storageKey="discord-theme"
 					>
-						<ModalProvider />
-						{children}
+						<SocketProvider>
+							<ModalProvider />
+							{children}
+						</SocketProvider>
 					</ThemeProvider>
 				</body>
 			</html>
--- types.ts
+++ types.ts
@@ -1,7 +1,19 @@
-import { Member, Profile, Server } from "@prisma/client";
+import { Server as NetServer, Socket } from 'net';
+import { NextApiResponse } from 'next';
+import { Server as SocketIOServer } from 'socket.io';
+
+import { Member, Profile, Server } from '@prisma/client';
 
 export type ServerWhitMembersWithProfiles = Server & {
-  members: (Member & {
-    profile: Profile
-  })[]
+	members: (Member & {
+		profile: Profile;
+	})[];
+};
+
+export type NextApiResponseServerIo = NextApiResponse & {
+	socket: Socket & {
+		server: NetServer & {
+			io: SocketIOServer;
+		};
+	};
 };
