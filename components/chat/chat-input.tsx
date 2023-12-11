2ab0c152050eb7cbdbb5aecc1f25ec79eef0fcfc
Felix Garcia <felix.garcia@contractor.temenos.com>
2023-12-11 15:07:11 -0500

    Add emoji picker component

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -5,14 +5,15 @@ import axios from 'axios';
 import qs from 'query-string';
 
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { Plus, Smile } from 'lucide-react';
+import { Plus } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 
 import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
 
 import { Input } from '@/components/ui/input';
 import { useModal } from '@/hooks/use-modal-store';
+import { EmojiPicker } from '@/components/emoji-picker';
 
 interface ChatInputProps {
 	apiUrl: string;
@@ -27,6 +28,7 @@ const formSchema = z.object({
 
 export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
 	const { onOpen } = useModal();
+	const router = useRouter();
 
 	const form = useForm<z.infer<typeof formSchema>>({
 		resolver: zodResolver(formSchema),
@@ -35,7 +37,7 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
 		},
 	});
 
-	const isLoading = form.formState.isLoading;
+	const isLoading = form.formState.isSubmitting;
 
 	const onSubmit = async (values: z.infer<typeof formSchema>) => {
 		try {
@@ -45,6 +47,8 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
 			});
 
 			await axios.post(url, values);
+			form.reset();
+			router.refresh();
 		} catch (error) {
 			console.log(error);
 		}
@@ -67,19 +71,19 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
 										<Plus className="text-white dark:text-[#313338]" />
 									</button>
 									<Input
+										{...field}
 										disabled={isLoading}
 										className="px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200"
 										placeholder={`Message ${type === 'conversation' ? name : '#' + name}`}
-										{...field}
 									/>
 									<div className="absolute top-7 right-8">
-										<Smile />
+										<EmojiPicker onChange={(emoji: string) => field.onChange(`${field.value} ${emoji}`)} />
 									</div>
 								</div>
 							</FormControl>
 						</FormItem>
 					)}
-				></FormField>
+				/>
 			</form>
 		</Form>
 	);
