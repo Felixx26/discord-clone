d5b539c847b5eb87af4d9dea2e074d67a615cf4a
Felix Garcia <felix.garcia@contractor.temenos.com>
2023-12-16 01:14:41 -0500

    Add useChatSocket hook to handle chat socket events

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -5,6 +5,7 @@ import { format } from 'date-fns';
 import { Member, Message, Profile } from '@prisma/client';
 import { Loader2, ServerCrash } from 'lucide-react';
 import { useChatQuery } from '@/hooks/use-chat-query';
+import { useChatSocket } from '@/hooks/use-chat-socket';
 import { ChatWelcome } from './chat-welcome';
 import { ChatItem } from './chat-item';
 
@@ -40,12 +41,17 @@ export const ChatMessages = ({
 	type,
 }: ChatMessagesProps) => {
 	const queryKey = `chat:${chatId}`;
+	const addKey = `chat:${chatId}:messages`;
+	const updateKey = `chat:${chatId}:messages:update`;
+
 	const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } = useChatQuery({
 		queryKey,
 		apiUrl,
 		paramKey,
 		paramValue,
 	});
+
+	useChatSocket({ queryKey, addKey, updateKey });
 
 	if (status === 'pending') {
 		return (
