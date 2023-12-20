d25311121009004667ea1ae9251e08232db7bc1d
Felix Garcia <felix.garcia@contractor.temenos.com>
2023-12-19 22:38:45 -0500

    Update Pusher configuration

--- lib/pusher.ts
+++ lib/pusher.ts
@@ -3,11 +3,11 @@ import PusherClient from 'pusher-js';
 
 export const pusherServer = new PusherServer({
 	appId: process.env.PUSHER_APP_ID!,
-	key: process.env.PUSHER_APP_KEY!,
+	key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
 	secret: process.env.PUSHER_APP_SECRET!,
-	cluster: process.env.PUSHER_APP_CLUSTER!,
+	cluster: process.env.NEXT_PUBLIC_PUSHER_APP_CLUSTER!,
 });
 
-export const pusherClient = new PusherClient('a257abe078040a479ea1', {
-	cluster: 'mt1',
+export const pusherClient = new PusherClient(process.env.NEXT_PUBLIC_PUSHER_APP_KEY!, {
+	cluster: process.env.NEXT_PUBLIC_PUSHER_APP_CLUSTER!,
 });
